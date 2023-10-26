4cd8e6b72216a8ab05c7804ca0006370309a6e7c
Ngọ Văn Long <75620455+thuandiep177@users.noreply.github.com>
2023-10-26 21:01:33 +0700

    Create translator.py

---
+++ translator.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from textblob import TextBlob
+from tkinter import ttk, messagebox
+from googletrans import Translator, LANGUAGES
+
+root=Tk()
+root.title("Translate - 207CT47832 - Ngọ Văn Long")
+root.geometry("460x580")
+root.iconphoto(False, PhotoImage(file="Google_Translate_logo.svg.png"))
+
+root.mainloop()
